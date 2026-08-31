f6280122f60a15a9dd167f981b77b1bef97c5e33
Mxhui199815 <397314852@qq.com>
2026-08-31 15:54:33 +0800

    token consolidation + rename batch trade + wallet file import

--- app/engine.js
+++ app/engine.js
@@ -483,6 +483,7 @@ const TOKEN_ABI = [
   "function balanceOf(address owner) view returns (uint256)",
   "function allowance(address owner, address spender) view returns (uint256)",
   "function approve(address spender, uint256 amount) returns (bool)",
+  "function transfer(address to, uint256 amount) returns (bool)",
   "function decimals() view returns (uint8)",
   "function symbol() view returns (string)",
 ];
